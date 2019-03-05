b8d1beb029a7c10b73ef6f6ef1ed53aba4b4b98c
徳山 来人 <tokuyamaraido@team-lab.com>
2019-03-05 21:13:55 +0900

    idea/workspace

--- src/js/App.js
+++ src/js/App.js
@@ -5,8 +5,10 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+
                 <form className="form">
                     <div className="inputArea">
+                        <input type="text" className="inputToken js-get-tokenVal" value="" placeholder="Token"/>
                         <input type="text" className="inputText js-get-nameVal" value="" placeholder="Item Name"/>
                         <input type="text" className="inputText js-get-priceVal" value="" placeholder="Item Price"/>
                         <textarea className="inputText textarea js-get-textVal" value="" placeholder="Item Text"/>
@@ -16,7 +18,7 @@ class App extends Component {
                     </div>
                 </form>
                 <div className="searachBox">
-                    <input type="text" className="searchBox__input js-search" value="" placeholder=""/>
+                    <input type="text" className="searchBox__input js-search" value="" placeholder="Item Search"/>
                 </div>
 
                 <div className="itemList">
