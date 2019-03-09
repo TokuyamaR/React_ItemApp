8d2bcc2a0a27c4bff90730ab65630009e0fb37ee
徳山 来人 <qpflapper.1007@gmail.com>
2019-03-09 18:34:50 +0900

    190309 [add]検索機能

--- src/js/App.js
+++ src/js/App.js
@@ -1,20 +1,87 @@
-import React, {Component} from 'react';
-import '../App.css';
-import ItemCreator from './components/ItemCreator'
-import {ItemList} from './components/ItemList'
+import React from 'react';
+import ItemCreator from './components/ItemCreator';
+import ItemSearch from './components/ItemSearch';
+import {ItemList} from './components/ItemList';
+import _ from 'lodash';
+import '../css/item.css'
 
-class App extends Component {
+class App extends React.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            data: [
+                {
+                    id: this.createHashId(),
+                    name: 'sample item1',
+                    price: 100,
+                    text: 'sample1',
+                    img: 'img1'
+                },
+                {
+                    id: this.createHashId(),
+                    name: 'sample item2',
+                    price: 200,
+                    text: 'sample2',
+                    img: 'img2'
+                }
+            ],
+            searchText: ''
+        };
+
+        this.callBackRemoveItem = this.callBackRemoveItem.bind(this);
+        this.callBackAddItem = this.callBackAddItem.bind(this);
+        this.callBackSearch = this.callBackSearch.bind(this);
+        this.filterCollection = this.filterCollection.bind(this);
+    }
+
+    createHashId(){
+        return Math.random().toString(36).slice(-16);
+    }
+
+    callBackSearch(searchVal){
+        this.setState({
+            searchText: searchVal
+        });
+    }
+
+    callBackRemoveItem(id){
+        let data = _.reject(this.state.data, {'id': id});
+        this.setState({
+            data: data
+        });
+    }
+
+
+    callBackAddItem(name, price, text, img){
+        let nextData = this.state.data;
+        nextData.push({id: this.createHashId(), name: name, price: price, text: text, img: img});
+        this.setState({
+            data: nextData
+        });
+    }
+
+    filterCollection(elm){
+        const regexp = new RegExp('^' + this.state.searchText, 'i');
+        return (elm.name.match(regexp));
+    }
+
     render() {
+
+        const data = (this.state.searchText) ? this.state.data.filter(this.filterCollection) : this.state.data;
+
         return (
-            <div className="App">
+            <div className="main">
+                <h1 className="mainTitle">React App</h1>
+                <div className="app">
 
-                <ItemCreator/>
-                <div className="searachBox">
-                    <input type="text" className="searchBox__input js-search" value="" placeholder="Item Search"/>
-                </div>
+                    <ItemCreator callBackAddItem={this.callBackAddItem}/>
 
-                <div className="itemList">
-                    <ItemList/>
+                    <ItemSearch callBackSearch={this.callBackSearch}/>
+
+                    <div className="itemList">
+                        <ItemList data={data} callBackRemoveTask={this.callBackRemoveItem}/>
+                    </div>
                 </div>
             </div>
         );
--- src/js/components/ItemList.js
+++ src/js/components/ItemList.js
@@ -1,54 +1,36 @@
 import React from 'react';
 import Item from './Item';
-import _ from 'lodash';
 
 //登録された商品リストを包括するコンポーネント
 export class ItemList extends React.Component{
     constructor(props){
         super(props);
-        this.state = {
-            data: [
-                {
-                    id: 0,
-                    name: 'sample item1',
-                    price: 100,
-                    text:'sample1',
-                    img:'img1'
-                },
-                {
-                    id: 1,
-                    name: 'sample item2',
-                    price: 200,
-                    text:'sample2',
-                    img:'img2'
-                }
-            ]
-        };
         this.handleRemove = this.handleRemove.bind(this);
     }
 
     handleRemove(id){
-        let data = _.reject(this.state.data,{'id':id});
-        this.setState({
-            data: data
-        });
+        this.props.callBackRemoveItem(id);
     }
     render(){
         let items = [];
-        for(let i in this.state.data){
-            items.push(<Item key={this.state.data[i].id}
-                             id={this.state.data[i].id}
-                             name={this.state.data[i].name}
-                             price={this.price.data[i].price}
-                             text={this.state.data[i].text}
-                             img={this.state.data[i].img}
+        for(let i in this.props.data){
+            items.push(<Item key={this.props.data[i].id}
+                             id={this.props.data[i].id}
+                             name={this.props.data[i].name}
+                             price={this.props.data[i].price}
+                             text={this.props.data[i].text}
+                             img={this.props.data[i].img}
             />);
         }
 
         return (
-            <ul className="list js-item_list">
-                {items}
-            </ul>
+            <div>
+                <div className="item_list-title left">Item List</div>
+
+                <ul className="list js-item_list">
+                    {items}
+                </ul>
+            </div>
         );
     }
 }
