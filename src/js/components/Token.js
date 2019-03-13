b7e6a8cc19338f2a4eaa31c33566983c073cb636
徳山 来人 <qpflapper.1007@gmail.com>
2019-03-13 22:15:56 +0900

    190313 [add]Tokenコンポーネント

---
+++ src/js/components/Token.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export class TokenInput extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            token:'',
+            token_errMsg:''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            token:e.target.value;
+        });
+    }
+
+    render(){
+
+        return(
+            <input type="text" className="token" name="token" placeholder="token" value={this.state.token} onChange={this.handleChange}/>
+        );
+    }
+
+}
